f9ac47aa8203e6534800cb6df9f8bd6a40ee9353
ljz <920595557@qq.com>
2022-11-08 21:17:18 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,15 +4,14 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    // if my_option.is_none() {
-    // my_option.unwrap();
-    // }
+    if my_option.is_none() {
+        panic!();
+    }
 
-    let my_arr = &[-1, -2, -3, -4, -5, -6];
+    let my_arr = &[-1, -2, -3 - 4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
     vec![1, 2, 3, 4, 5].resize(0, 5);
-    // println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
