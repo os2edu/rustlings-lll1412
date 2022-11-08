775da88a66314dcaf6560873260ec4f5e10f3096
lll1412 <920595557@qq.com>
2022-11-08 21:25:43 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -11,7 +11,8 @@ fn main() {
     let my_arr = &[-1, -2, -3 - 4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
-    vec![1, 2, 3, 4, 5].resize(0, 5);
+    let my_empty_vec = vec![1, 2, 3, 4, 5].clear();
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
